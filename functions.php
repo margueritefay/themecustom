2d2811cbaf7835296afe266c7760d71dcb9a5611
Unknown <marguerite.fay.mf@gmail.com>
2019-03-25 20:46:25 +0100

    mise en place d'options accessible et droits utilisateurs

--- functions.php
+++ functions.php
@@ -24,15 +24,65 @@ function themecustom_scripts(){
 
 add_action('wp_enqueue_scripts', 'themecustom_scripts');
 
-//chargement dans le panneau admin
-function themecustom_admin_scripts(){
 
-  //style dans le panneau d'admin
-  wp_enqueue_style('themecustom_adm-core', get_template_directory_uri().'/css/bootstrap.min.css',
-  array(), 'THEMECUSTOM_VERSION', 'all');
-} //fin fonction chargement des styles dans panneau d'admin
+//================================================================
+//===================== CHARGEMENT DES STYLES ET SCRIPTS
+//================================================================
+function themecustom_admin_init(){
 
-add_action('admin_init', 'themecustom_admin_scripts');
+//********** action 1
+  function themecustom_admin_scripts(){
+
+    //style dans le panneau d'admin
+    wp_enqueue_style('themecustom_adm-core', get_template_directory_uri().'/css/bootstrap.min.css',
+    array(), 'THEMECUSTOM_VERSION', 'all');
+  } //fin fonction chargement des styles dans panneau d'admin
+
+  add_action('admin_enqueue_scripts', 'themecustom_admin_scripts');
+} //fin de la fonction themecustom_admin_scripts
+
+add_action('admin_init', 'themecustom_admin_init');
+
+
+
+//================================================================
+//===================== ACTIVATION DES OPTIONS
+//================================================================
+
+function themecustom_activate_options(){
+  $theme_options = get_option('tc_options');
+  if(!$theme_options){
+    $options = array(
+      'image_01_url' =>'',
+      'legend_01'    =>''
+    );
+    add_option('tc_options', $options);
+  }
+}
+//after_switch_theme permet d'executer la fonction uniquement lors
+//de l'activation du thème.
+add_action('after_switch_theme', 'themecustom_activate_options');
+
+
+//================================================================
+//===================== MENU OPTIONS DU THEME
+//================================================================
+function themecustom_admin_menus(){
+  add_menu_page(
+    'Themecustom Options',
+    'Options du thème',
+    'publish_pages',
+    'tc_theme_opts',
+    'tc_build_options_page'
+  );
+
+  //on inclu le fichier on se trouve la fonction tc_build_options_page()
+  include('includes/build-options-page.php');
+
+}//fin fonction themecustom_admin_menus
+
+add_action('admin_menu', 'themecustom_admin_menus');
+
 
 //================================================================
 //===================== UTILITAIRES
---
+++ includes/build-options-page.php
@@ -0,0 +1,6 @@
+<?php
+
+function tc_build_options_page(){
+  echo'<h1> Hello les options</h1>';
+}//fin de la fonction tc_build_options_page
+ ?>
